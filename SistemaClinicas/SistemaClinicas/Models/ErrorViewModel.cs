bdb457bd3afa19a0f5f55225a62b41d977d0e07d
Alvaro Deguer <alvarodeguer@gmail.com>
2022-06-05 21:00:16 -0300

    Creacion del proyecto ASP.NET Core MVC

---
+++ SistemaClinicas/SistemaClinicas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaClinicas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
