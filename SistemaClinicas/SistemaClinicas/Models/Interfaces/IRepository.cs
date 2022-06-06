10121fb0565c78103bba7327a37ab31005705851
Alvaro Deguer <alvarodeguer@gmail.com>
2022-06-06 16:22:51 -0300

    Agregando DB Agregada Intefaz para manejar repositorios Agregados algunos botones de prueba

---
+++ SistemaClinicas/SistemaClinicas/Models/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaClinicas.Models.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetEntity(int id);
+        List<T> GetEntities();
+        void AddEntity(T entity);
+        void DeleteEntity(int id);
+        void EditEntity(T entity);
+    }
+}
